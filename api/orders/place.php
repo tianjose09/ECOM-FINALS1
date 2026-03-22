6bf131b0da699fbd5682c3059c329c7f15133fea
tianjose09 <202310267@fit.edu.ph>
2026-03-22 13:29:04 +0800

    changed the directory for the database  in cart (changable pa)

--- api/orders/place.php
+++ api/orders/place.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../utils/database.utils.php';
 require_once __DIR__ . '/../helpers/auth.php';
 require_once __DIR__ . '/../helpers/response.php';
 
